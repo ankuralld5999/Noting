10c705c8eb6dff36a85514765147d699109e9b85
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2022-09-13 14:55:21 +0530

    Updates

---
+++ precache-manifest.f7eac7308981e3f1e4f362667d4e38a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee8c198b88258fc9b9e0a3a9700f2b0",
+    "url": "/Noting/index.html"
+  },
+  {
+    "revision": "66bda0722348e7a42142",
+    "url": "/Noting/static/js/2.953e52f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Noting/static/js/2.953e52f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81507bdd738aacacd836",
+    "url": "/Noting/static/js/main.c575ec20.chunk.js"
+  },
+  {
+    "revision": "9f8eb780f1c38a46e804",
+    "url": "/Noting/static/js/runtime-main.f84dc216.js"
+  }
+]);
